4603a42e30d388a02ead7543adbce12ea640dfa7
chaitanyaratakondla <chaitanya.ratakondla@kellogg.northwestern.edu>
2022-02-01 15:42:52 +0000

    Updated 1-models code along

--- code-along/1-models.rb
+++ code-along/1-models.rb
@@ -10,11 +10,47 @@ Company.destroy_all
 
 # 1a. check out the schema file
 # 1b. check out the model file
+puts "There are now #{Company.all.count} companies."
 
 # 2. create new companies
+values = 
+{name:"Assassins Co.",
+url: "assassinscreed.com",
+city: "Aleppo",
+state: "Syria"}
+acreed = Company.new(values)
+acreed.save
+
+values = 
+{name:"Rainbow corp.",
+url: "residentevil.com",
+city: "Chicago",
+state: "Illinois"}
+rainbow = Company.new(values)
+rainbow.save
+
+puts "There are now #{Company.all.count} companies."
 
 # 3. query companies table
+# Removed this line to check below: puts Company.all.inspect
+syrian_company = Company.where({state:"Syria"})[0]
+# 0 is to take only 1 value
+puts syrian_company.inspect
+
 
 # 4. read column values from row
+puts syrian_company.read_attribute(:url)
+puts syrian_company.url
 
 # 5. update attribute value
+
+syrian_company.write_attribute(:slogan, "Where Eagles Dare")
+syrian_company.slogan = "Where Eagles Dare"
+syrian_company.save
+
+puts syrian_company.inspect
+
+new_company = Company.new
+new_company.name = "Weyland-Yutani"
+new_company.url = "aliens.com"
+new_company
