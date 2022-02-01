46c0f5f7bf088e104af3c29ea0b2254ce7dc5490
chaitanyaratakondla <chaitanya.ratakondla@kellogg.northwestern.edu>
2022-02-01 14:51:58 +0000

    First code along - basics of classes & methods

--- code-along/0-classes.rb
+++ code-along/0-classes.rb
@@ -3,6 +3,23 @@
 # should be the top-level directory (i.e. /workspace/your-app/) and then run:
 # rails runner code-along/0-classes.rb
 
+fav_food = "Biriyani"
+fav_food2 = "Ramen"
+puts fav_food.upcase
+puts fav_food2.upcase
+
+no_of_food = 5
+puts no_of_food.class
+
 # String Class
+class Dog
+    def speak
+        puts "Woof"
+    end
+end
+
+rusty = Dog.new 
+rusty.speak
+rusty.sit
 
 # String Methods
